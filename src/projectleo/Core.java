d27ff29a644a1246de1b775e63ddcf32517712f9
Alan Williams <astropiloto@gmail.com>
2012-01-11 09:14:04 -0500

    Added code base.

---
+++ src/projectleo/Core.java
@@ -0,0 +1,34 @@
+package projectleo;
+
+/**
+ * This is the main class for project LEO.
+ * @author alan
+ */
+public class Core {
+    /**
+     * This method allows the ProjectLEO class to implement the core class.
+     */
+    public static void Core() {
+}
+
+/**
+ * This method displays the intro information.
+ */
+public static void intro(){
+        System.out.println("Welcome to the Project LEO compiler!");
+        System.out.println("Please enter the code line by line.");
+        System.out.println("Press enter after each line.");
+}
+/**
+ * This method displays a tutorial for new users.
+ */
+public static void tutorial(){
+    // TODO Insert tutorial
+}
+/**
+ * This method sets up the compiler for compiling.
+ */
+public static void setup(){
+    // TODO Insert Setup
+}
+}
---
+++ src/projectleo/ProjectLEO.java
@@ -0,0 +1,19 @@
+package projectleo;
+
+/**
+ * Project LEO is a compiler designed for academic use.
+ * @author alan
+ */
+public class ProjectLEO {
+
+    /**
+     * @param args the command line arguments
+     * @comment Please do not insert any actual commands here!
+     */
+    public static void main(String[] args) {
+        Core core = new Core();
+        Core.intro();
+        Core.tutorial();
+        Core.setup();
+    }
+}
